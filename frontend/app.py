4c0bcb77465a232bdd558c480d38e223998570a7
kannanvasan-aiml <kannanvasan@gmail.com>
2026-09-20 20:36:00 +0530

    Add frontend/app.py

---
+++ frontend/app.py
@@ -0,0 +1,64 @@
+import streamlit as st
+import pandas as pd
+import requests
+
+# Base URL of the Flask backend
+BACKEND_URL =  "http://backend:7860"
+
+# Set the title of the Streamlit app
+st.title("SuperKart System")
+
+# Section for online prediction
+st.subheader("Online Prediction")
+
+
+# Input fields for product and store data
+Product_Weight = st.number_input("Product Weight", min_value=0.0, value=12.66)
+Product_Sugar_Content = st.selectbox("Product Sugar Content", ["Low Sugar", "Regular", "No Sugar"])
+Product_Allocated_Area = st.number_input("Product Allocated Area", min_value=0.0, value=0.027)
+Product_MRP = st.number_input("Product MRP", min_value=0.0, value=117.08)
+Store_Size = st.selectbox("Store Size", ["Small", "Medium", "High"])
+Store_Location_City_Type = st.selectbox("Store Location City Type", ["Tier 1", "Tier 2", "Tier 3"])
+Store_Type = st.selectbox("Store Type", ["Supermarket Type1", "Supermarket Type2", "Supermarket Type3", "Departmental Store","Food Mart"])
+Product_Id_char = st.selectbox("Product ID Character", ["FD", "DR", "NC"])
+Store_Age_Years = st.number_input("Store Age (Years)", min_value=0, value=16)
+Product_Type_Category = st.selectbox("Product Type Category", ["Perishables", "Non Perishables"])
+
+product_data = {
+    "Product_Weight": Product_Weight,
+    "Product_Sugar_Content": Product_Sugar_Content,
+    "Product_Allocated_Area": Product_Allocated_Area,
+    "Product_MRP": Product_MRP,
+    "Store_Size": Store_Size,
+    "Store_Location_City_Type": Store_Location_City_Type,
+    "Store_Type": Store_Type,
+    "Product_Id_char": Product_Id_char,
+    "Store_Age_Years": Store_Age_Years,
+    "Product_Type_Category": Product_Type_Category
+}
+
+if st.button("Predict", type='primary'):
+    response = requests.post(f"{BACKEND_URL}v1/predict", json=product_data)
+    if response.status_code == 200:
+        result = response.json()
+        predicted_sales = result["Sales"]
+        st.write(f"Predicted Product Store Sales Total: ₹{predicted_sales:.2f}")
+    else:
+        st.error("Error in API request")
+
+# Section for batch prediction
+st.subheader("Batch Prediction")
+
+# Allow users to upload a CSV file for batch prediction
+uploaded_file = st.file_uploader("Upload CSV file for batch prediction", type=["csv"])
+
+# Make batch prediction when the "Predict Batch" button is clicked
+if uploaded_file is not None:
+    if st.button("Predict Batch", type="primary"):
+        response = requests.post("f"{BACKEND_URL}v1/batch_predict", files={"file": uploaded_file})  # Send file to Flask API
+        if response.status_code == 200:
+            predictions = response.json()
+            st.success("Batch predictions completed!")
+            st.write(predictions)  # Display the predictions
+        else:
+            st.error("Unable to connect to the prediction API.")
